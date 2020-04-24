d175b0d755079198eb5ca22d3d505211db067757
Hậu Phan Trọng <19520077@ms.uit.edu.vn>
2020-04-24 09:58:32 +0700

    So phuc

---
+++ Bai1/SP.cpp
@@ -0,0 +1,55 @@
+#include "SP.h"
+#include<iostream> 
+using namespace std;
+SP::SP(float newfThuc = 0, float newfAo = 0)
+{
+	this->fThuc = newfThuc;
+	this->fAo = newfAo;
+}
+SP::~SP(){}
+void SP::Nhap()
+{
+	cout << "Nhap Phan thuc = "; cin >> this->fThuc;
+	cout << "Nhap Phan ao = "; cin >> this->fAo;
+	
+}
+void SP::Xuat()
+{
+	if (fThuc == 0)
+	{
+		if (fAo != 0)
+		{
+			if (fAo < 0)
+				cout << " - " << -fAo << "i" << endl;
+			else
+				cout << fAo << "i" << endl;
+		}
+		else cout << "0" << endl;
+	}
+	else
+	{
+		if (fAo != 0)
+		{
+			if (fAo < 0)
+				cout << fThuc << " - " << -fAo << "i" << endl;
+			else
+				cout << fThuc << " + " << -fAo << "i" << endl;
+		}
+		else cout << fThuc << endl;
+
+	}
+}
+SP SP::operator+(SP a)
+{
+	SP res;
+	res.fThuc = this->fThuc + a.fThuc;
+	res.fAo = this->fAo + a.fAo;
+	return res;
+}
+SP operator-(SP a, SP b)
+{
+	SP res;
+	res.fThuc = this->fThuc + a.fThuc;
+	res.fAo = this->fAo + a.fAo;
+	return res;
+}
